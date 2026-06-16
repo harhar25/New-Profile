b0ecde19d6daa26aad01323c3fd8f51fafca1a89
Harold Jey Nahid Madjos <haroldjeymadjos@gmail.com>
2026-06-17 06:58:17 +0800

    Fixes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  outputFileTracingRoot: process.cwd(),
+  webpack: (config, { isServer }) => {
+    return config;
+  },
 };
 
 export default nextConfig;
